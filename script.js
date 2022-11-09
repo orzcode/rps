8e0422c891f88a046d6f0dd4e25df61178ac87c1
kambostrong <71000311+kambostrong@users.noreply.github.com>
2022-11-09 17:05:41 +1300

    cleanup code, prepare for DOM Div

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-var totalgames = 0;
-var yourscore = 0;
-var computerscore = 0;
-var gamecount = 0;
-let playerSelection
+//Initializing variables
+let totalgames = 0;
+let yourscore = 0;
+let computerscore = 0;
+let gamecount = 0;
+let playerSelection;
+////
 
+//DOM Manipulation
 const rock = document.querySelector('#rock');
 rock.addEventListener("click", () => playerSelection = "ROCK")
 rock.addEventListener("click", game);
@@ -16,13 +19,13 @@ const scissors = document.querySelector('#scissors');
 scissors.addEventListener("click", () => playerSelection = "SCISSORS")
 scissors.addEventListener("click", game);
 
+////
 
 
-
-
+//<!--  Here is the full game function --!>//
 function game(){
- //   for (var gamecount = 0; gamecount < 5; gamecount++) {
-//
+
+//Randomly assigns a computer choice
 function getComputerChoice(){
     switch (Math.floor(Math.random() * 3))
     {
@@ -36,8 +39,9 @@ function getComputerChoice(){
     return;
 }
 const computerSelection = getComputerChoice();
+////
 
-//
+//Runs the round based on the two 'inputs'
 function playRound(playerSelection, computerSelection){
     if (playerSelection == "ROCK"){
         switch (computerSelection){
@@ -68,31 +72,40 @@ function playRound(playerSelection, computerSelection){
     }
 }
 let result = playRound(playerSelection, computerSelection);
-//
-// totalgames = totalgames + 1;
-// if (result === "You win!") {
-//     yourscore = yourscore + 1;
-// }   else if (result === "You lose!") {
-//     computerscore = computerscore + 1;
-// }
-//
+////
+
+//totalgames counter - and respective score incrementer
+totalgames = totalgames + 1;
+if (result === "You win!") {
+    yourscore = yourscore + 1;
+}   else if (result === "You lose!") {
+    computerscore = computerscore + 1;
+}
+////
+
+//Logging section - DOM these, but maybe keep them too for debugging
 console.log("You chose: " + playerSelection);
 console.log("The computer chose: " + computerSelection);
 console.log("The result of this match was: " + result);
-//console.log("Total games so far: " + totalgames);
-//console.log("Your score is: " + yourscore);
-//console.log("Computer score is: " + computerscore);
-alert("Round complete! Click for next round");
-//
+console.log("Total games so far: " + totalgames);
+console.log("Your score is: " + yourscore);
+console.log("Computer score is: " + computerscore);
+////
+
+//Calculting final outcome - only once 5 totalgames reached - then resets totalgames and respective scores
+if (totalgames == 5) {
+    if (yourscore > computerscore){
+    console.log("Match complete! Your score was " + yourscore + " compared to the computer's " + computerscore + " which means you win!");
+}   else if (yourscore < computerscore){
+    console.log("Match complete! Your score was " + yourscore + " compared to the computer's " + computerscore + " which means you lose!");
+}   else if (yourscore == computerscore){
+    console.log("Match complete! Your score was " + yourscore + " compared to the computer's " + computerscore + " which means you draw!");
 }
-//calculting final outcome - after core loop
-// if (gamecount == 5) {
-//     if (yourscore > computerscore){
-//     console.log("Match complete! Your score was " + yourscore + " compared to the computer's " + computerscore + " which means you win!");
-// }   else if (yourscore < computerscore){
-//     console.log("Match complete! Your score was " + yourscore + " compared to the computer's " + computerscore + " which means you lose!");
-// }   else if (yourscore == computerscore){
-//     console.log("Match complete! Your score was " + yourscore + " compared to the computer's " + computerscore + " which means you draw!");
-// }}
-//
-// }
+    totalgames = 0;
+    yourscore = 0;
+    computerscore = 0;
+};
+////
+}
+////
+
