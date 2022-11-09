891c7d8adddec43b1faecbea7cd0666727fda494
kambostrong <71000311+kambostrong@users.noreply.github.com>
2022-11-09 16:11:21 +1300

    clickable buttons working, stage 1 before loop

--- script.js
+++ script.js
@@ -2,10 +2,26 @@ var totalgames = 0;
 var yourscore = 0;
 var computerscore = 0;
 var gamecount = 0;
+let playerSelection
+
+const rock = document.querySelector('#rock');
+rock.addEventListener("click", () => playerSelection = "ROCK")
+rock.addEventListener("click", game);
+
+const paper = document.querySelector('#paper');
+paper.addEventListener("click", () => playerSelection = "PAPER")
+paper.addEventListener("click", game);
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener("click", () => playerSelection = "SCISSORS")
+scissors.addEventListener("click", game);
+
+
+
+
 
 function game(){
  //   for (var gamecount = 0; gamecount < 5; gamecount++) {
-        //the below will be part of the loop (these are core game functions)
 //
         function getComputerChoice(){
     switch (Math.floor(Math.random() * 3))
@@ -23,10 +39,9 @@ function game(){
     return;
 }
 const computerSelection = getComputerChoice();
+
 //
-//let playerSelection = prompt("Choose a symbol!", "Rock, paper, or scissors");
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toUpperCase();
     if (playerSelection == "ROCK"){
         switch (computerSelection){
             case "Scissors":
@@ -76,11 +91,10 @@ let result = playRound(playerSelection, computerSelection);
 console.log("You chose: " + playerSelection);
 console.log("The computer chose: " + computerSelection);
 console.log("The result of this match was: " + result);
-console.log("Total games so far: " + totalgames);
-console.log("Your score is: " + yourscore);
-console.log("Computer score is: " + computerscore);
+//console.log("Total games so far: " + totalgames);
+//console.log("Your score is: " + yourscore);
+//console.log("Computer score is: " + computerscore);
 alert("Round complete! Click for next round");
-//Note: would like to make console display these stats before next round, instead of all at once at game end
 //
 }
 //calculting final outcome - after core loop
@@ -95,17 +109,6 @@ alert("Round complete! Click for next round");
 //
 // }
 
-game();
+//game();
 
 //
-const rock = document.querySelector('#rock');
-
-rock.addEventListener("click", playRound());
-
-const scissors = document.querySelector('#scissors');
-
-scissors.addEventListener("click", playRound());
-
-const paper = document.querySelector('#paper');
-
-paper.addEventListener("click", playRound());
